b64b8fcf8720a2a4661bb119efefa5a888197345
Robin Green <robin@designingreen.co.uk>
2025-05-22 18:04:27 +0100

    Update footer

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 							<img style="height: 150px;" class="mw-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/ss-logo.png">
 						</figure>
                 		<p>Find us on <a href="https://www.linkedin.com/company/sort-survive-ltd/" target="_blank" title="Sort & Survive on LinkedIn">LinkedIn</a>&nbsp;|&nbsp;<a href="<?php echo get_page_link( get_page_by_path( 'privacy' ) ); ?>">Privacy Policy</a>&nbsp;|&nbsp;<a href="<?php echo get_page_link( get_page_by_path( 'terms-and-conditions' ) ); ?>">Terms &amp; Conditions</a></p>
-                		<p class="ss-copyright">&copy;&nbsp;<?php echo date('Y'); ?> Sort & Survive Ltd. All rights reserved.</p>
+                		<p class="ss-copyright">&copy;&nbsp;<?php echo date('Y'); ?> Sort & Survive Ltd - Limited Company registered in England and Wales (Company number 16026311). All rights reserved.</p>
                 	</div>
                 </div>
             </div>
